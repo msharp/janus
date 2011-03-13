cdd1067f35b582aae2d47c23297281b6ea89305f
Max Sharples <maxsharples@gmail.com>
2011-03-13 23:04:46 +1100

    generate route uses querystring param

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-
 var connect = require('connect')
     , express = require('express')
     , sys = require('sys')
-    , io = require('socket.io')
     , port = (process.env.PORT || 8081);
 
 var server = express.createServer();
@@ -14,14 +12,15 @@ server.get('/', function(req,res){
   res.send("Hello World!");
 });
 
-server.get('/:link', function(req,res){
-    res.send("Get uri for shorturl key: " + req.params.link);
+server.get('/generate', function(req,res){
+    res.send("Generate shorturl for: " + req.query.url);
 });
 
-server.get('/generate/:uri', function(req,res){
-    res.send("Generate shorturl for: " + req.params.uri);
+server.get('/:link', function(req,res){
+    res.send("Get uri for shorturl key: " + req.params.link);
 });
 
 server.listen(port);
 
 console.log('Listening on http://0.0.0.0:' + port );
+
