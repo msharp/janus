b32ec6166d070ba66543dc2bf00d42608f4f02bf
Max Sharples <maxsharples@gmail.com>
2011-03-13 22:12:47 +1100

    app with 2 routes for url make/redirect

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ var connect = require('connect')
 var server = express.createServer();
 server.configure(function(){
   server.set('root', __dirname)
-})
+});
 
 server.get('/', function(req,res){
   res.send("Hello World!");
-})
+});
+
+server.get('/:link', function(req,res){
+    res.send("Get uri for shorturl key: " + req.params.link);
+});
+
+server.get('/generate/:uri', function(req,res){
+    res.send("Generate shorturl for: " + req.params.uri);
+});
 
 server.listen(port);
 
